3ba0bc5ecde79775794dae444c24699eb6e7f975
sohil <sohil@FefeDell.dhcp.wustl.edu>
2024-02-21 15:49:32 -0600

    flagg

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,33 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Color green = new Color(81, 137, 34);
+		Color pink = new Color(255, 182, 193);
+		StdDraw.setPenColor(pink);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.25);
+		StdDraw.setPenColor(green);
+		StdDraw.rectangle(0.5, 0.5, 0.5, 0.25);
+		double[] xr1 = {0,0.25,1,1} ;
+		double[] yr1 = {0.25,0.25, 0.65,0.75};
+		StdDraw.setPenColor(green);
+		StdDraw.filledPolygon(xr1,yr1);
+		double[] xr2 = {0.5,0.6,1,1} ;
+		double[] yr2 = {0.75,0.75, 0.45,0.3};
+		StdDraw.filledPolygon(xr2,yr2);
+		StdDraw.setPenColor(Color.white);
 		
+		double[] xt2 = {0.6,0.8,0.9} ;
+		double[] yt2 = {0.25,0.35, 0.25};
+		StdDraw.filledPolygon(xt2, yt2);
+		StdDraw.setPenColor(green);
+		StdDraw.polygon(xt2, yt2);
+		StdDraw.setPenColor(Color.white);
+		
+		double[] xt1 = {0.5,0.65,0.75} ;
+		double[] yt1 = {0.25,0.35, 0.25};
+		StdDraw.filledPolygon(xt1, yt1);
+	
+		StdDraw.setPenColor(green);
+		StdDraw.polygon(xt1, yt1);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,8 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -19,6 +21,27 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+	
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		boolean isfilled = in.nextBoolean();
+
+		
+		Color given = new Color(r,g,b);
+		System.out.println(r + g + b);
+		
+		if(shape.equals("rectangle")) {
+			double[] parameter = new double[4];
+			for (int i=0; i<4; i++) {
+				parameter[i]= in.nextDouble();
+		}
+			StdDraw.setPenColor(given);
+			StdDraw.rectangle(parameter[0], parameter[1], parameter[2], parameter[3]);
+		
+		}
+		
 		
 	}
 }
